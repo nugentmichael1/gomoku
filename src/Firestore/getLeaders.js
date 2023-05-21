5ebaaaf564282c20cf378cf77fc72ebf772cdca5
Michael Nugent <NugentMichael@live.com>
2023-05-20 23:26:20 -0700

    Leaderboard start

---
+++ src/Firestore/getLeaders.js
@@ -0,0 +1,37 @@
+
+
+import firebase from "firebase/compat/app"
+
+import "firebase/compat/firestore"
+
+import "../FirebaseConfig"
+
+const db = firebase.firestore()
+
+const getLeaders = async (field, direction) => {
+
+    const usersRef = db.collection("Users")
+
+    //direction: "desc" or "ascend" or null?
+    const leadersQuery = usersRef.orderBy(field, direction).limit(10)
+
+    const leadersResult = await leadersQuery.get()
+        .then((querySnapshot) => {
+            const leadersArr = querySnapshot.docs.map((doc) => {
+                return doc.data()
+            })
+
+            return leadersArr
+        })
+        .catch((error) => {
+            console.error("Error in getLeaders():", error)
+            return null
+        })
+
+    console.log(leadersResult)
+
+    return leadersResult
+
+}
+
+export default getLeaders;
--- src/Pages/Leaderboard.js
+++ src/Pages/Leaderboard.js
@@ -1,26 +1,84 @@
-import React from 'react'
+import React, { useState, useEffect } from "react"
+
+import getLeaders from "../Firestore/getLeaders"
+
+//css
+import "../CSS/leaderboard.css"
 
 //check session storage for leaderboard data
 //if false, download from backend
+
+const timePlayedConverter = (seconds) => {
+
+  //transform to minutes
+  const minutes = seconds / 60
+
+  const secondsCopy = seconds % 60
+
+  console.log(minutes, secondsCopy)
+}
+
+
+const buildLeadersTable = (data) => {
+
+  if (data === null) return
+
+  const tbody = []
+
+  data.forEach(({ username, won_games, time_played, games_played }) => {
+
+    const tr = []
+
+    //username
+    tr.push(<td key={"username"}>{username}</td>)
+
+    //won_games
+    tr.push(<td key={"won_games"}>{won_games}</td>)
+
+    //time_played
+    tr.push(<td key={"time_played"}>{time_played}</td>)
+
+    timePlayedConverter(time_played)
+
+    //games_played
+    tr.push(<td key={"games_played"}>{games_played}</td>)
+
+    tbody.push(<tr key={username}>{tr}</tr>)
+  })
+
+  return <tbody>{tbody}</tbody>
+}
 
 function Leaderboard({ user }) {
 
   //use user object to fill in user table
 
-  return (<>
-    <h1>Leaderboard</h1>
-    <ul>
+  const [leadersTableBody, setLeadersTableBody] = useState(null)
 
-      <li>
-        <p>
-          Please pardon my mess while this project's codebase undergoes a translation from LAMP to MERN. (April 27, 2023)
-        </p>
-      </li>
-      <li>
-        <p>Just need to implement backend capabilities. (May 12, 2023)</p>
-      </li>
-    </ul>
-  </>
+  useEffect(() => {
+    async function fetchData() {
+      const leaders = await getLeaders("won_games", "desc")
+      setLeadersTableBody(buildLeadersTable(leaders))
+
+      return leaders
+    }
+    fetchData()
+  }, [])
+
+  return (<div id="leaderboard">
+    <h1>Leaderboard</h1>
+    <table id="leaders">
+      <thead>
+        <tr>
+          <th>User</th>
+          <th>Games Won</th>
+          <th>Total Time Played</th>
+          <th>Total Games Played</th>
+        </tr>
+      </thead>
+      {leadersTableBody}
+    </table>
+  </div>
   )
 }
 
