51516d04f0de7afff5db5752202aa4abda781d1c
Michael Nugent <NugentMichael@live.com>
2024-07-10 13:14:46 -0700

    NPM updates

--- functions/recordGame.js
+++ functions/recordGame.js
@@ -13,7 +13,7 @@ const jwtUtility = require('./jwtUtility')
 
 const recordGame = functions.https.onRequest((res, req) => {
     cors(req, res, () => {
-
+        /* I think I moved away from serverless functions and this feature is in the firestore folder of the frontend, now. */
     })
 })
 
